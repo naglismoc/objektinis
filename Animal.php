907a86c6ed8c5c5b897980fdae077461086784db
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2020-10-07 19:09:15 +0300

    obj2

--- Animal.php
+++ Animal.php
@@ -1,15 +1,25 @@
 <?php
 
 class Animal{
-    public $name;
+    private $name;
     public $terrain;
     public $legs;
+    static public $statId = 0;
+    public $id;
 
     public function __construct($name="Kengura", $terrain="zemumos", $legs=2,$weight=70){
         $this->name=$name;
         $this->terrain=$terrain;
         $this->legs=$legs;
         $this->weight = $weight;
+        $this->id=$this::$statId;
+        $this::$statId++;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name=$name;
     }
     public function value(){
         return ($this->weight*120);
--- index.php
+++ index.php
@@ -4,43 +4,23 @@ session_unset();
 require __DIR__."/Animal.php";
 
 $kengura = new Animal("skipis","grasslands",4,150);
-// $kengura->name="skipis";
-// var_dump($kengura);
-echo "<br>";
-$kengura->value(70);
-// var_dump($kengura);
-echo "<br>";
-// $kengura->toString();
-$kotletas = new Animal("kotletas","dykumos",2,70);
-// $kotletas->toString();
-// echo $kotletas->weight;
-// echo $kotletas->value();
-// var_dump($kotletas);
-echo "<br>";
-$_SESSION['kotletas']=$kotletas;
-$_SESSION['kotletas']->name="greituolis";
-// var_dump($_SESSION['kotletas']);
-
-echo "<br>";
-// var_dump($kotletas);
-$k3 = new Animal();
-// var_dump($k3);
-$zooSodas=[];
-for ($i=0; $i < 10; $i++) { 
-   array_push($zooSodas,new Animal("kangaroo".$i,"field".$i,2,rand(26,66)));
-}
-var_dump($zooSodas);
-echo "<br>";
-echo "<br>";
-for ($i=0; $i <count($zooSodas) ; $i++) { 
-    for ($a=0; $a <count($zooSodas)-1 ; $a++) { 
-       if($zooSodas[$a]->weight < $zooSodas[$a+1]->weight){
-            $tempAroo = $zooSodas[$a];
-            $zooSodas[$a]=$zooSodas[$a+1];
-            $zooSodas[$a+1]=$tempAroo;
-       }
-    }
-}
-var_dump($zooSodas);
-
+$kengura2 = new Animal("dupis","grasslands",4,150);
+var_dump($kengura);
+echo $kengura->legs;
+echo "<br><br>";
+// $kengura::$id=21;
+echo $kengura->id;  
+echo "<br><br>";
+echo $kengura2->id; 
+echo "<br><br>"; 
+echo Animal::$statId;
+echo "<br><br>";
+echo $kengura2::$statId; 
+echo "<br><br>";
+// echo $kengura->name;
+echo "<br><br>";
+echo $kengura->getName();
+echo $kengura->setName("skiparisas");
+echo "<br><br>";
+echo $kengura->getName();
 ?>
